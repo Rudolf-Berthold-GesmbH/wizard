c57ca6af05933148f94326f44c898662edf3b156
Oliver <oliversergej@gmail.com>
2024-12-27 14:35:19 +0100

    add AnimateGrid component and update avatars.vue for dynamic logo display

--- pages/avatars.vue
+++ pages/avatars.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>
-    test
+  <div class="flex items-center justify-center p-4">
+    <AnimateGrid :cards :rotateY="0" :rotateX="0">
+      <template #logo="{ logo }">
+        <img
+          class="logo mx-auto h-30 w-auto"
+          :src="logo"
+        />
+      </template>
+    </AnimateGrid>
   </div>
 </template>
 
 <script lang="ts" setup>
 
+const cards = Array.from({ length: 16 }, () => ({
+  logo: '/images/avatars/Alex-2.png',
+}));
 </script>
-
-<style>
-
-</style>
