d6598a04058422d2db8d54db0280582691c7a161
Oliver <oliversergej@gmail.com>
2024-12-27 14:05:54 +0100

    refactor app.vue layout and enhance RadiantText component with additional styling and content

--- app.vue
+++ app.vue
@@ -8,22 +8,29 @@
       class="absolute inset-0 flex items-center justify-center px-8"
     >
       <div class="flex size-full max-w-6xl flex-col md:flex-row">
-        <div class="m-auto flex flex-col gap-4">
-          <div class="flex flex-col gap-2">
+        <div class="m-auto flex flex-col gap-3">
+          <div class="flex flex-col">
             <RadiantText
-              class="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400"
+              class="inline-flex flex-col items-center justify-center px-4 py-0 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400"
               :duration="5"
             >
-              <span class="text-3xl font-bold">🧙🏻‍♂️ Berthold Wizard</span>
+              <span class="text-6xl font-bold">🧙🏻‍♂️</span>
+              <span class="text-3xl font-bold">Berthold Wizard</span>
+              <span class="text-xs font-light">©2024 Edition</span>
             </RadiantText>
           </div>
           <div class="z-10 flex flex-col gap-2">
             <RainbowButton disabled>Spielen</RainbowButton>
             <RainbowButton disabled>Leaderboard</RainbowButton>
-            <RainbowButton>Anleitung</RainbowButton>
+            <RainbowButton is="a" href="/files/anleitung.pdf">Anleitung</RainbowButton>
           </div>
-          <div class="flex justify-center">
-            <AnimatedTooltip :items="people" />
+          <div class="flex flex-col py-2">
+            <div class="flex">
+              <AnimatedTooltip :items="people" />
+            </div>
+            <Nuxt-Link to="/about" class="m-auto">
+              <span class="font-light text-neutral-600">more</span>
+            </Nuxt-Link>
           </div>
         </div>
       </div>
